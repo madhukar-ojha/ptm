48639358f7ffefe625ac23c3f1e966d50e6aa1cf
Madhukar Ojha <73935081+madhukar-ojha@users.noreply.github.com>
2020-12-02 19:23:38 +0530

    ptm-web project divided into 3 modules. ptm-main, ptm-login and ptm-common to designed into spring microservices. Now old project ptm-web deleted.
    Signed-off-by: Madhukar Ojha <73935081+madhukar-ojha@users.noreply.github.com>

--- ptm-web/setup/database/ptm/General.sql
+++
@@ -1,4 +0,0 @@
-SELECT m.menuKey, m.menuName, m.menuUrl, mc.actionKey, mc.actionType, mc.subMenuKey, mc.subMenuName, mc.subMenuUrl, 
-mc.commandKey, mc.commandName FROM submenu_command mc INNER JOIN designation_submenu_command dmc 
-ON mc.id = dmc.submenu_command_id INNER JOIN menu m ON m.id = mc.menu_id 
-WHERE dmc.designation_id =30 ORDER BY m.sequence;
--- ptm-web/setup/database/ptm/trigger.sql
+++
@@ -1,5 +0,0 @@
-CREATE TRIGGER employee_BeforeInsert BEFORE INSERT ON `employee`
-FOR EACH ROW SET NEW.createdOn = CURDATE();
-
-CREATE TRIGGER employee_BeforeUpdate BEFORE UPDATE ON `employee`
-FOR EACH ROW SET NEW.updatedOn = CURDATE();
